7452407dc626f061a86069e3ac98b60b4af85abf
Moth <moth.feedback@gmail.com>
2023-07-02 23:11:01 +0300

    was changes some images

--- sell_by_car/main/urls.py
+++ sell_by_car/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_page_render)
+    # Потом вместо полной ссылки в href просто ссылаемся на параметр name данной ссылки
+    # (p.s. 21:15 https://www.youtube.com/watch?v=OYeqcxaYUbQ&list=PLDyJYA6aTY1nZ9fSGcsK4wqeu-xaJksQQ&index=5)
+    path('', views.main_page_render, name="home")
 ]
