0c811a8d21f83731f9810016f6606039cd9f2b5c
Moth <moth.feedback@gmail.com>
2023-07-04 15:57:42 +0300

    add models cars

--- sell_by_car/main/models.py
+++ sell_by_car/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Django автоматически добавляет первичный ключ, если тот не был добавлен вручную
+
+class Cars(models.Model):
+    brand = models.CharField(max_length=15)
+    model = models.CharField(max_length=30)
+    year = models.CharField(max_length=4)
+
