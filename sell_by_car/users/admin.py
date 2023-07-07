0bb18beab5cc1d033b408a7160b4560a5f78c77f
Moth <moth.feedback@gmail.com>
2023-07-07 18:50:26 +0300

    я в психушку (отменил, всё, что делал последние 2 дня). у меня сгорела жопа.

--- sell_by_car/users/admin.py
+++ sell_by_car/users/admin.py
@@ -1,41 +1,12 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import CustomUser
+# from .forms import CustomUserCreationForm, CustomUserChangeForm
+#
+#
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     model = CustomUser
 
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-
-    add_fieldsets = (
-        *UserAdmin.add_fieldsets, (
-            'Custom fields',
-            {
-                'fields': (
-                    'birth_date',
-                    'phone_number',
-                )
-
-            }
-
-
-        )
-    )
-
-    fieldsets = (
-        *UserAdmin.fieldsets, (
-            'Custom fields',
-            {
-                'fields': (
-                    'birth_date',
-                    'phone_number',
-                )
-
-            }
-
-
-        )
-    )
 
 # admin.site.register(Users, UsersAdmin) = @admin.register(Users)
--- sell_by_car/users/forms.py
+++ sell_by_car/users/forms.py
@@ -1,16 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
-
-
-class CustomUserCreationForm(UserCreationForm):
-
-    class Meta:
-        model = CustomUser
-        fields = ('birth_date', 'phone_number')
-
-
-class CustomUserChangeForm(UserChangeForm):
-
-    class Meta:
-        model = CustomUser
-        fields = ('birth_date', 'phone_number')
+# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+# from .models import CustomUser
+#
+#
+# class CustomUserCreationForm(UserCreationForm):
+#
+#     class Meta:
+#         model = CustomUser
+#         fields = ('birth_date', 'phone_number')
+#
+#
+# class CustomUserChangeForm(UserChangeForm):
+#
+#     class Meta:
+#         model = CustomUser
