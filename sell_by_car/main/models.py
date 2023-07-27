d86790846acda42f5c3c2018a8cfca65cdfffd3f
Moth <moth.feedback@gmail.com>
2023-07-27 04:32:00 +0300

    validators, models, tests, pip, img.
    temporary_domains (validate_email) -> tuple, birthday validation update, International dialing country codes, phone number validation update, new tests have been added to check the validation of data. Updated pip, deleted old html and css files and folders associated with them.
    
    I tried to move the validators to a separate file, but Django started to do nonsense.
    
    Temporarily moved the images folder to the "under_development" directory.
    
    In this commit, all 31 tests pass successfully.

--- sell_by_car/main/models.py
+++ sell_by_car/main/models.py
@@ -7,31 +7,108 @@ import datetime
 # python manage.py makemigrations
 # python manage.py migrate
 
+# International dialing country codes for auto call detection
+country_phone_codes = {'Australia': '+61', 'Austria': '+43', 'Azerbaijan': '+994', 'Albania': '+355', 'Algeria': '+213',
+                       'Angola': '+244', 'Andorra': '+376', 'Antigua and Barbuda': '+1268', 'Argentina': '+54',
+                       'Armenia': '+374', 'Afghanistan': '+93', 'Bahamas': '+1242', 'Bangladesh': '+880',
+                       'Barbados': '+1246', 'Bahrain': '+973', 'Belarus': '+375', 'Belize': '+501', 'Belgium': '+32',
+                       'Benin': '+229', 'Bulgaria': '+359', 'Bolivia': '+591', 'Bosnia and Herzegovina': '+ 387',
+                       'Botswana': '+267', 'Brazil': '+55', 'Brunei': '+673', 'Burkina Faso': '+226',
+                       'Burundi': '+257', ' Bhutan': '+975', 'Vanuatu': '+678', 'Vatican City': '+39',
+                       'Great Britain': '+44', 'Hungary': '+36',
+                       'Venezuela': '+ 58', 'East Timor': '+670', 'Vietnam': '+84', 'Gabon': '+241', 'Haiti': '+509',
+                       'Guyana': '+592', ' Gambia': '+220', 'Ghana': '+233', 'Guatemala': '+502', 'Guinea': '+224',
+                       'Guinea-Bissau': '+245', 'Germany': '+49', 'Honduras': '+504', 'Grenada': '+1473',
+                       'Greece': '+30',
+                       'Georgia': '+995', 'Denmark': '+45', 'Djibouti': '+253', 'Dominica': '+1767',
+                       'Dominican Republic': '+1809', 'Egypt': '+20', 'Zambia': '+260', 'Zimbabwe': '+263',
+                       'Israel': '+972', 'India': '+91', 'Indonesia': '+62', 'Jordan': '+962', 'Iraq': '+964',
+                       'Iran': '+98', 'Ireland': '+353', 'Iceland': '+354', 'Spain': '+34', 'Italy': '+39',
+                       'Yemen': ' +967', 'Cape Verde': '+238', 'Kazakhstan': '+77', 'Cambodia': '+855',
+                       'Cameroon': '+237',
+                       'Canada': '+1', 'Qatar': '+974', 'Kenya': '+254', 'Cyprus': '+357', 'Kyrgyzstan': '+996',
+                       'Kiribati': '+686', 'China': '+86', 'Colombia': '+57', 'Comoros': '+269',
+                       'Congo, Democratic Republic': '+243', 'Congo, Republic': '+242', 'Costa Rica ': '+506',
+                       'Ivory Coast': '+225', 'Cuba': '+53', 'Kuwait': '+965', 'Laos': '+856', 'Latvia': '+371',
+                       'Lesotho': '+266', 'Liberia': '+231', 'Lebanon': '+961', 'Libya': '+218', 'Lithuania': '+370',
+                       'Liechtenstein': '+423', 'Luxembourg': '+352', 'Mauritius': '+230', 'Mauritania': '+222',
+                       'Madagascar': '+261', 'Macedonia': '+389', 'Malawi': '+265', 'Malaysia': '+60', 'Mali': '+223',
+                       'Maldives': '+960', 'Malta': '+356', 'Morocco': '+212', 'Marshall Islands': '+692',
+                       'Mexico': '+52', 'Mozambique': '+259', 'Moldova': '+373', 'Monaco': '+377', 'Mongolia': '+976',
+                       'Myanmar': '+95', 'Namibia': '+264', 'Nauru': '+674', 'Nepal': '+977', 'Niger': '+227',
+                       'Nigeria': '+234', 'Netherlands': '+31', 'Nicaragua': '+505', 'New Zealand': '+64',
+                       'Norway': '+47', 'United Arab Emirates': '+971', 'Oman': '+968', 'Pakistan': '+92',
+                       'Palau': '+680', 'Panama': '+507', 'Papua New Guinea': '+675', 'Paraguay': '+595',
+                       'Peru': '+51', 'Poland': '+48', 'Portugal': '+351', 'Russia': '+7', 'Rwanda': '+250',
+                       'Romania': '+40', 'El Salvador': '+503', 'Samoa': '+685', 'San Marino': '+378',
+                       'Sao Tome and Principe': '+239', 'Saudi Arabia': '+966', 'Swaziland': '+268',
+                       'North Korea': '+850', 'Seychelles ': '+248', 'Senegal': '+221',
+                       'Saint Vincent and the Grenadines': '+1784', 'Saint Kitts and Nevis': '+1869',
+                       'Saint Lucia': '+ 1758',
+                       'Serbia': '+381', 'Singapore': '+65', 'Syria': '+963', 'Slovakia': '+421', 'Slovenia': '+986',
+                       'United States of America': '+1', 'Solomon Islands': '+677', 'Somalia': '+252', 'Sudan': '+249',
+                       'Suriname': '+597', 'Sierra Leone': '+232', 'Tajikistan': '+992', 'Thailand': '+66',
+                       'Tanzania': '+255', 'Togo': '+228', 'Tonga': '+676', 'Trinidad and Tobago': '+1868',
+                       'Tuvalu': '+688', 'Tunisia': '+216', 'Turkmenistan': '+993', 'Turkey': '+90', ' Uganda': '+256',
+                       'Uzbekistan': '+998', 'Ukraine': '+380', 'Uruguay': '+598',
+                       'Federated States of Micronesia': '+691', 'Fiji': '+679', 'Philippines': '+63',
+                       'Finland': '+358', 'France': '+33', 'Croatia': '+385', 'Central African Republic': '+ 236',
+                       'Chad': '+235', 'Montenegro': '+381', 'Czech Republic': '+420', 'Chile': '+56',
+                       'Switzerland': '+41', 'Sweden ': '+46', 'Sri Lanka': '+94', 'Ecuador': '+593',
+                       'Equatorial Guinea': '+240', 'Eritrea': '+291', 'Estonia': '+372', 'Ethiopia': '+251',
+                       'South Korea': '+82', 'South Africa': '+27', 'Jamaica': '+1876', 'Japan': ' +81'}
 
-def validate_phone_number(value):
-    if not re.match(r'^\+\d{10,18}$', value):
+
+# Format phone number: +7 800 555 35 35 (without spaces) -> +78005553535 (in this form will be in the database)
+# No dividers - to save memory and lightly search.
+# Separators for convenience can be inserted when extracting the phone number.
+def validate_phone_number(value: str):
+    if not re.match(r'^\+\d{11,13}', value) or value[:-10] not in country_phone_codes.values():
         raise ValidationError("Invalid phone number")
 
 
-def validate_email(value):
-    temporary_domains = ["dispostable.com", "guerrillamail.com", "mailinator.com", "10minutemail.com", "tempmail.net",
+# Format email address:
+def validate_email(value: str):
+    temporary_domains = ("dispostable.com", "guerrillamail.com", "mailinator.com", "10minutemail.com", "tempmail.net",
                          "yopmail.com", "temp-mail.org", "trashmail.com", "getnada.com", "burnermail.io", "maildrop.cc",
                          "fakeinbox.com", "mytrashmail.com", "10mails.net", "mailnesia.com", "sharklasers.com",
                          "mailcatch.com", "wegwerfmail.de", "spamgourmet.com", "mailinator.net", "discard.email",
                          "getairmail.com", "owlymail.com", "jetable.org", "throwawaymail.com", "tempail.com",
                          "mailsac.com", "tempmailo.com", "mailtemp.uk", "gettempmail.com", "trashmail.ws",
                          "maildrop24.com", "temp-mail.io", "throwawaymail.com", "guerrillamail.net", "example.com"
-                         ]
+                         )
+
     if not re.match(rf"^[A-Za-z0-9._%+-]+@(?!({'|'.join(temporary_domains)})).*$", value):
         raise ValidationError("Invalid email address")
 
 
-# repair solution
-def validate_dob(value):
+# Format date day of birth: dd.mm.yyyy or dd/mm/yyyy
+def validate_dob(value: str):
+    def is_leap_year(_value):
+        if _value % 4 != 0:
+            return False
+        elif _value % 100 != 0:
+            return True
+        elif _value % 400 != 0:
+            return False
+        else:
+            return True
+
     try:
-        if not (len(value) != 10 and value[2] == '.' and value[5] == '.' and
-                18 <= int(value[-4:]) > datetime.datetime.now().year - 80):
+        if not re.match(r"(0[1-9]|[12][0-9]|3[01])[/.](0[1-9]|1[012])[/.]((19|20)\d{2})", value):  # dd.mm.yyyy
             raise ValidationError("Invalid date of birth")
+
+        validate_dob_day, validate_dob_month, validate_dob_year = map(int, re.split(r'[/.]', value))
+
+        if validate_dob_month == 2:  # # Verification for the number of February
+            if validate_dob_day > 29 or (validate_dob_day == 29 and not is_leap_year(validate_dob_year)):
+                raise ValidationError("Invalid date of birth")
+
+        # If younger than 16, then without the consent of the parents of this person we have no right
+        # to process his personal data. And the upper bar in 120 is necessary to check the correctness of the input.
+        if 16 <= int(str(datetime.datetime.today())[:4]) - validate_dob_year >= 120:
+            raise ValidationError("Invalid date of birth")
+
     except ValueError:
         raise ValidationError("wtf with year")
 
@@ -89,7 +166,7 @@ class Cars(models.Model):
     to_100 = models.DecimalField(max_digits=5,
                                  decimal_places=2,
                                  validators=[RegexValidator(r'^\d{1,3}\.\d{2}$',
-                                             message="1-3 digits before dot and 2 digits after dot")
+                                                            message="1-3 digits before dot and 2 digits after dot")
                                              ],
                                  )
 
--- sell_by_car/main/tests/test_validators.py
+++ sell_by_car/main/tests/test_validators.py
@@ -10,13 +10,34 @@ class ExtraUserTestCase(TestCase):
         self.assertFalse(self._check_test_phone_number_validation("+7-800-555-35-35"))
 
     def test_phone_number_validation_2(self):
-        self.assertTrue(self._check_test_phone_number_validation("+88005553535"))
+        self.assertFalse(self._check_test_phone_number_validation("+88005553535"))
 
     def test_phone_number_validation_3(self):
         self.assertFalse(self._check_test_phone_number_validation("+7 800 555 35 35"))
 
     def test_phone_number_validation_4(self):
         self.assertFalse(self._check_test_phone_number_validation("8 800 555 35 35"))
+
+    def test_phone_number_validation_5(self):
+        self.assertTrue(self._check_test_phone_number_validation("+18448724681"))
+
+    def test_phone_number_validation_6(self):
+        self.assertFalse(self._check_test_phone_number_validation("+(254)0204920000"))
+
+    def test_phone_number_validation_7(self):
+        self.assertFalse(self._check_test_phone_number_validation("+(254)-020-492-00-00"))
+
+    def test_phone_number_validation_8(self):
+        self.assertFalse(self._check_test_phone_number_validation("8448724681"))
+
+    def test_phone_number_validation_9(self):
+        self.assertFalse(self._check_test_phone_number_validation("88005506576"))
+
+    def test_phone_number_validation_10(self):
+        self.assertTrue(self._check_test_phone_number_validation("+78005506576"))
+
+    def test_phone_number_validation_11(self):
+        self.assertTrue(self._check_test_phone_number_validation("+2540204920000"))
 
     @staticmethod
     def _check_test_phone_number_validation(value):
@@ -28,7 +49,7 @@ class ExtraUserTestCase(TestCase):
 
     # E-mail
     def test_email_validation_1(self):
-        self.assertTrue(self._check_test_email_validation("fedorv1234@gmail.com"))
+        self.assertTrue(self._check_test_email_validation("alexeyPopov1234@gmail.com"))
 
     def test_email_validation_2(self):
         self.assertTrue(self._check_test_email_validation("createsuperuser@mail.ru"))
@@ -45,29 +66,56 @@ class ExtraUserTestCase(TestCase):
         return True
 
     # Date of Birth
-    # def test_dob_validation_1(self):
-    #     self.assertTrue(self._check_test_dob_validation("01.01.2000"))
+    def test_dob_validation_1(self):
+        self.assertTrue(self._check_test_dob_validation("01.01.2000"))
 
-    # def test_dob_validation_2(self):
-    #     self.assertFalse(self._check_test_dob_validation("00.00.1995"))
-    #
-    # def test_dob_validation_3(self):
-    #     self.assertFalse(self._check_test_dob_validation("30.02.1990"))
-    #
-    # def test_dob_validation_4(self):
-    #     self.assertTrue(self._check_test_dob_validation("23.05.1950"))
-    #
-    # def test_dob_validation_5(self):
-    #     self.assertFalse(self._check_test_dob_validation("12.01.1940"))
-    #
-    # def test_dob_validation_6(self):
-    #     self.assertFalse(self._check_test_dob_validation("01.01.2120"))
+    def test_dob_validation_2(self):
+        self.assertFalse(self._check_test_dob_validation("00.00.1995"))
 
-    # def test_dob_validation_7(self):
-    #     self.assertFalse(self._check_test_dob_validation("12.6.2001"))
+    def test_dob_validation_3(self):
+        self.assertFalse(self._check_test_dob_validation("30.02.1990"))
+
+    def test_dob_validation_4(self):
+        self.assertTrue(self._check_test_dob_validation("23.05.1962"))
+
+    def test_dob_validation_5(self):
+        self.assertTrue(self._check_test_dob_validation("12.01.1985"))
+
+    def test_dob_validation_6(self):
+        self.assertFalse(self._check_test_dob_validation("01.01.2120"))
+
+    def test_dob_validation_7(self):
+        self.assertFalse(self._check_test_dob_validation("12.6.2001"))
 
     def test_dob_validation_8(self):
-        self.assertFalse(self._check_test_dob_validation("01.01.2020"))
+        self.assertTrue(self._check_test_dob_validation("01.01.2000"))
+
+    def test_dob_validation_9(self):
+        self.assertTrue(self._check_test_dob_validation("29.02.2020"))
+
+    def test_dob_validation_10(self):
+        self.assertFalse(self._check_test_dob_validation("29.02.2021"))
+
+    def test_dob_validation_11(self):
+        self.assertFalse(self._check_test_dob_validation("31.02.2003"))
+
+    def test_dob_validation_12(self):
+        self.assertFalse(self._check_test_dob_validation("23.05.0123"))
+
+    def test_dob_validation_13(self):
+        self.assertFalse(self._check_test_dob_validation("32.05.1999"))
+
+    def test_dob_validation_14(self):
+        self.assertFalse(self._check_test_dob_validation("23.05.1899"))
+
+    def test_dob_validation_15(self):
+        self.assertTrue(self._check_test_dob_validation("31.07.1985"))
+
+    def test_dob_validation_16(self):
+        self.assertFalse(self._check_test_dob_validation("6.05.2001"))
+
+    def test_dob_validation_17(self):
+        self.assertFalse(self._check_test_dob_validation("6.5.2001"))
 
     @staticmethod
     def _check_test_dob_validation(value):
@@ -76,4 +124,3 @@ class ExtraUserTestCase(TestCase):
         except ValidationError:
             return False
         return True
-
